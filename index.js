0fb728733735cb77d6b831482d0c84818344f67d
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2022-05-05 11:27:18 -0400

    Improvements

--- index.js
+++ index.js
@@ -1,82 +1,145 @@
-const fs = require('fs');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const fs = require("fs");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-const cheerio = require('cheerio');
-const fetch = require('node-fetch');
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
 
-const {BUILDKITE_API_TOKEN} = process.env;
+const { BUILDKITE_API_TOKEN } = process.env;
 if (!BUILDKITE_API_TOKEN) {
-    throw new Error("Missing required env var BUILDKITE_API_TOKEN");
+  throw new Error("Missing required env var BUILDKITE_API_TOKEN");
 }
 
+const MAX_ARTIFACT_SIZE = 1024 * 1024 * 4; // Arbitrary 4Mb max size on artifacts
+
+/** @typedef {{job_id: string, filename: string, download_url: string}} Artifact */
+
+/**
+ * Parse a job URL like https://buildkite.com/my-org/my-pipeline/builds/430653
+ * Converts it to {orgSlug: "my-org", pipelineSlug: "my-pipeline", buildNumber: "430653"}
+ * @param {string} jobUrl
+ * @returns {{orgSlug: string, pipelineSlug: string, buildNumber: string}}
+ */
 function parseJobUrl(jobUrl) {
-    const url = new URL(jobUrl);
-    const match = url.pathname.match(/\/(?<orgSlug>.+)\/(?<pipelineSlug>.+)\/builds\/(?<buildNumber>\d+)/);
-    return match.groups;
+  const url = new URL(jobUrl);
+  const match = url.pathname.match(
+    /\/(?<orgSlug>.+)\/(?<pipelineSlug>.+)\/builds\/(?<buildNumber>\d+)/
+  );
+  return match.groups;
 }
 
-/** @returns {Promise<{job_id: string, filename: string, download_url: string}[]>} */
+/**
+ * Fetch a list of artifacts for a given job
+ * Focuses only on junit.xml artifacts
+ * @param {string} jobUrl
+ * @returns {Promise<Artifact[]>}
+ */
 async function fetchListOfArtifacts(jobUrl) {
-    const {orgSlug, pipelineSlug, buildNumber} = parseJobUrl(jobUrl);
-    const resp = await fetch(
-        `https://api.buildkite.com//v2/organizations/${orgSlug}/pipelines/${pipelineSlug}/builds/${buildNumber}/artifacts?per_page=100`,
-        {headers: {'Authorization': `Bearer ${BUILDKITE_API_TOKEN}`}
-    });
-    return await resp.json();
+  const { orgSlug, pipelineSlug, buildNumber } = parseJobUrl(jobUrl);
+  const resp = await fetch(
+    `https://api.buildkite.com//v2/organizations/${orgSlug}/pipelines/${pipelineSlug}/builds/${buildNumber}/artifacts?per_page=100`,
+    { headers: { Authorization: `Bearer ${BUILDKITE_API_TOKEN}` } }
+  );
+  const data = await resp.json();
+  return data.filter((artifact) => artifact.filename === "junit.xml");
 }
 
-async function fetchArtifactContent(artifact) {
-    process.stderr.write(`Fetching ${artifact.download_url}...\n`);
-    const resp = await fetch(artifact.download_url, {
-        redirect: 'manual',
-        headers: {'Authorization': `Bearer ${BUILDKITE_API_TOKEN}`}
+/**
+ * Finds the "storage URL" of an artifact, which is the google cloud resource it can be downloaded from
+ * @param {Artifact} artifact
+ * @returns {Promise<string>}
+ */
+async function fetchStorageUrl(artifact) {
+  const resp = await fetch(artifact.download_url, {
+    redirect: "manual", // Don't follow the redirect, results in a Google Cloud login page
+    headers: { Authorization: `Bearer ${BUILDKITE_API_TOKEN}` },
+  });
+  const data = await resp.json();
+  return data["url"];
+}
+
+/**
+ * Fetch content from a storage URL
+ * @param {Artifact} artifact
+ * @returns {Promise<string>}
+ */
+async function fetchContent(storageUrl) {
+  const gsIdentifier = new URL(storageUrl).pathname.slice(1);
+  try {
+    const { stdout } = await exec(`gsutil cat gs://${gsIdentifier}`, {
+      maxBuffer: MAX_ARTIFACT_SIZE,
     });
-    const {url: storageUrl} = await resp.json();
-    const gsIdentifier = new URL(storageUrl).pathname.slice(1);
-    const {stdout} = await exec(`gsutil cat gs://${gsIdentifier}`, {maxBuffer: 1024 * 1024 * 5});
     return stdout;
+  } catch (e) {
+    process.stderr.write(
+      "[ERROR] Failed to run the `gsutil` command, you may need to install it or setup permissions\n"
+    );
+    throw e;
+  }
 }
 
+/**
+ * Find a list of test names from a Junit document
+ * @param {string} junit
+ * @returns {string[]}
+ */
 function extractTestNames(junit) {
-    const $ = cheerio.load(junit);
-    return $('testsuites > testsuite > testcase')
-        .toArray()
-        .map(node => node.attribs.classname)
+  const $ = cheerio.load(junit);
+  return $("testsuites > testsuite > testcase")
+    .toArray()
+    .map((node) => node.attribs.classname);
 }
 
-async function loadAllJunits(jobUrl) {
-    const cacheKey = jobUrl.replace(/[/:]/g, '_');
-    const cacheFile = `/tmp/${cacheKey}`;
+/**
+ * Use a JSON file as a cache, return the content of the cache if present, or fn() if not
+ * @template {Function} T
+ * @param {string} cacheKey
+ * @param {T} fn
+ * @returns {ReturnType<T>}
+ */
+async function withJsonCache(cacheFile, fn) {
+  try {
+    return JSON.parse(await fs.promises.readFile(cacheFile));
+  } catch (e) {
+    const result = await fn();
+    fs.promises.writeFile(cacheFile, JSON.stringify(result));
+    return result;
+  }
+}
 
-    try {
-        return JSON.parse(await fs.promises.readFile(cacheFile));
-    } catch {
-        const artifacts = await fetchListOfArtifacts(jobUrl);
-        const artifactContents = await Promise.all(
-            artifacts
-                .filter((artifact) => artifact.filename === 'junit.xml')
-                .map((artifact) => fetchArtifactContent(artifact))
-        );
+/**
+ * Fetch all artifacts from a given job URL, leveraging a cache to avoid re-downloading
+ * @param {string} jobUrl
+ * @returns {Promise<string[]>}
+ */
+async function fetchArtifactContents(jobUrl) {
+  const cacheKey = jobUrl.replace(/[/:]/g, "_");
+  const cacheFile = `/tmp/${cacheKey}`;
 
-        await fs.promises.writeFile(cacheFile, JSON.stringify(artifactContents));
-
-        return artifactContents;
-    }
+  return withJsonCache(cacheFile, async () => {
+    const artifacts = await fetchListOfArtifacts(jobUrl);
+    return Promise.all(
+      artifacts.map(async (artifact) => {
+        const storageUrl = await fetchStorageUrl(artifact);
+        const content = await fetchContent(storageUrl);
+        return content;
+      })
+    );
+  });
 }
 
 async function main() {
-    const jobUrl = process.argv[2];
-    if (!jobUrl) {
-        throw new Error("usage: node index.js BUILDKITE_JOB_URL")
-    }
+  const jobUrl = process.argv[2];
+  if (!jobUrl) {
+    throw new Error("usage: node index.js BUILDKITE_JOB_URL");
+  }
 
-    const junits = await loadAllJunits(jobUrl);
-    const testNames = junits.flatMap(junit => extractTestNames(junit));
-    
-    testNames
-        .sort()
-        .forEach(name => console.log(name));
+  const artifactContents = await fetchArtifactContents(jobUrl);
+  const testNames = artifactContents.flatMap((junit) =>
+    extractTestNames(junit)
+  );
+
+  testNames.sort().forEach((name) => console.log(name));
 }
 
 main();
